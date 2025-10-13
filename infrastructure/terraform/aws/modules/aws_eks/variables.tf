0711cdd0d2ac907dcf18e144cffa4996427200d3
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-10-13 20:20:40 +0530

    Modifies
    Removed the name and CIDR variable duplication.

--- infrastructure/terraform/aws/modules/aws_eks/variables.tf
+++ infrastructure/terraform/aws/modules/aws_eks/variables.tf
@@ -1,33 +1,29 @@
 variable "cluster_name" {
   type = string
+  description = "EKS cluster name"
 }
 
 variable "cluster_version" {
   type = string
+  description = "EKS Kubernetes version"
 }
 
 variable "vpc_id" {
   type = string
+  description = "VPC ID where EKS will be created"
 }
 
 variable "private_subnet_ids" {
   type = list(string)
+  description = "Private subnets for worker nodes"
 }
 
 variable "public_subnet_ids" {
   type = list(string)
+  description = "Public subnets for load balancers"
 }
 
 variable "node_group_desired_capacity" {
   type = number
-}
-
-variable "name" {
-  type = string
-  description = "Name prefix for VPC and subnets"
-}
-
-variable "cidr" {
-  type = string
-  description = "CIDR block for the VPC"
+  description = "Desired number of worker nodes"
 }

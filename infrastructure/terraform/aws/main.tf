fe6377b4f4dd7acdf3a78c7b69911a352cb1061f
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-10-13 17:15:46 +0530

    ADD
    Added the initial code for eks n vpc.

---
+++ infrastructure/terraform/aws/main.tf
@@ -0,0 +1,22 @@
+# -------------------------
+# Networking (VPC, Subnets)
+# -------------------------
+module "vpc" {
+  source = "${path.module}/modules/aws_network"
+
+  name = var.cluster_name
+  cidr = var.vpc_cidr
+}
+
+# -------------------------
+# EKS Cluster
+# -------------------------
+module "eks" {
+  source                     = "${path.module}/modules/aws_eks"
+  cluster_name               = var.cluster_name
+  cluster_version            = var.cluster_version
+  vpc_id                     = module.vpc.vpc_id
+  private_subnet_ids         = module.vpc.private_subnet_ids
+  public_subnet_ids          = module.vpc.public_subnet_ids
+  node_group_desired_capacity = var.node_group_desired_capacity
+}

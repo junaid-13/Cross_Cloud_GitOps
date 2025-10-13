0ac6a712d45c0220addbb3d4a7bbd76fc9e2d663
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-10-13 21:12:54 +0530

    Modified
    Removed  the eks-region & vpc region in output file.

--- infrastructure/terraform/aws/outputs.tf
+++ infrastructure/terraform/aws/outputs.tf
@@ -1,11 +1,3 @@
 output "eks_cluster_name" {
   value = module.eks.cluster_name
 }
-
-output "eks_region" {
-  value = module.eks.region
-}
-
-output "network_region" {
-  value = module.vpc.region
-}

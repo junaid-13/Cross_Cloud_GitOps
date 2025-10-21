920ee29558b00c107795d00b1c5195195249e207
SHA ZUFISHAN JUNAID <165144602+junaid-13@users.noreply.github.com>
2025-10-21 16:57:49 +0530

    Modified
    Fix formatting of region in backend.tf

--- infrastructure/terraform/aws/backend.tf
+++ infrastructure/terraform/aws/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "crosscloud-terraform-state"
     key            = "aws/eks/terraform.tfstate"
-    region         = us-east-1
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "crosscloud-terraform-locks"
   }

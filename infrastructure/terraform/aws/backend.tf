3cedf39032e4d3c61c6ae5126c0a55a661bb369a
junaid-13 <zufishanjunaid.1999@gmail.com>
2026-07-07 19:19:17 +0530

    Fixed the dependency error.

--- infrastructure/terraform/aws/backend.tf
+++ infrastructure/terraform/aws/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "aws/eks/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    use-lockfile   = true
+    use_lockfile   = true
   }
 }
